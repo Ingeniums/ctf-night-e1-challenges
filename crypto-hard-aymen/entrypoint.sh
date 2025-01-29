df0d1ab81d829124b49aa86130e04af2fb0964d1
Aymen-drid <aymnehocam@gmail.com>
2025-01-29 13:18:34 +0100

    modify permession for ent.sh

--- crypto-hard-aymen/entrypoint.sh
+++ crypto-hard-aymen/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-# Script execution variables
 EXEC="python3 script.py"
 PORT=1337
 
-# Start socat to serve the script
-exec socat -dd -T300 tcp-listen:"$PORT",reuseaddr,fork,keepalive,su=nobody exec:"$EXEC",stderr
+exec socat -dd -T300 tcp-listen:"$PORT",reuseaddr,fork,keepalive exec:"$EXEC",stderr
